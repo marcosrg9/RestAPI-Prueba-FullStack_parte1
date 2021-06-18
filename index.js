4db7ade2ead756cdf686c431e97be19cb6b4059f
Marcos Rodríguez <marcosylrg@gmail.com>
2021-06-18 21:42:55 +0200

    Enlace del JSDoc en index.js corregido

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
  * Prueba para FullStack Developer // Guadaltech - Sevilla.
  * Parte 1 - BackEnd RestAPI + Listar FrontEnd (y actualizar).
  * @author Marcos Rodríguez Yélamo <marcosylrg@gmail.com>
- * @link https://github.com/marcosrg9/RestAPI-Prueba-FullStack
+ * @link https://github.com/marcosrg9/RestAPI-Prueba-FullStack_parte1
  * 
  * Node 14.15.4 - (17 de Junio de 2021).
  */
